946715682e3fa90152fae589e9851504f08f9db8
Nishant Patel <nishantpatel1842@gmail.com>
2020-03-31 16:09:47 +1300

    NN from 31-3-20

---
+++ NN.py
@@ -0,0 +1,126 @@
+# Import Libraries
+import torch
+import torchvision.transforms as transforms
+import torchvision.datasets as datasets
+import torchvision.models as models
+import torch.nn as nn
+import torch.optim as optim
+import numpy as np
+from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
+
+# Specify transforms using torchvision.transforms as transforms library
+transformations = transforms.Compose([
+    transforms.Resize(255),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+])
+
+# Load in each dataset and apply transformations using the torchvision.datasets as datasets library
+train_set = datasets.ImageFolder("Train", transform=transformations)
+val_set = datasets.ImageFolder("Test", transform=transformations)
+
+# Put into a Dataloader using torch library
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=1, shuffle=True)
+val_loader = torch.utils.data.DataLoader(val_set, batch_size=1, shuffle=True)
+
+# Get pretrained model using torchvision.models as models library
+model = models.densenet161(pretrained=True)
+# Turn off training for their parameters
+for param in model.parameters():
+    param.requires_grad = False
+
+# Create new classifier for model using torch.nn as nn library
+classifier_input = model.classifier.in_features
+num_labels = 2
+classifier = nn.Sequential(nn.Linear(classifier_input, 64),
+                           nn.ReLU(),
+                           nn.Linear(64, 32),
+                           nn.ReLU(),
+                           nn.Linear(32, num_labels),
+                           nn.LogSoftmax(dim=1))
+# Replace default classifier with new classifier
+model.classifier = classifier
+
+# Find the device available to use using torch library
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+# Move model to the device specified above
+model.to(device)
+
+# Set the error function using torch.nn as nn library
+criterion = nn.NLLLoss()
+# Set the optimizer function using torch.optim as optim library
+optimizer = optim.Adam(model.classifier.parameters())
+
+# Training the Model
+epochs = 2
+for epoch in range(epochs):
+    train_loss = 0
+    val_loss = 0
+    accuracy = 0
+
+    # Training the model
+    model.train()
+    counter = 0
+    for inputs, labels in train_loader:
+        # Move to device
+        inputs, labels = inputs.to(device), labels.to(device)
+        # Clear optimizers
+        optimizer.zero_grad()
+        # Forward pass
+        output = model.forward(inputs)
+        # Loss
+        loss = criterion(output, labels)
+        # Calculate gradients (backpropogation)
+        loss.backward()
+        # Adjust parameters based on gradients
+        optimizer.step()
+        # Add the loss to the training set's rnning loss
+        train_loss += loss.item() * inputs.size(0)
+
+        # Print the progress of our training
+        counter += 1
+        print(counter, "/", len(train_loader))
+
+# Validating the Model
+
+    # Evaluating the model
+    model.eval()
+    counter = 0
+    # Tell torch not to calculate gradients
+    with torch.no_grad():
+        for inputs, labels in val_loader:
+            # Move to device
+            inputs, labels = inputs.to(device), labels.to(device)
+            # Forward pass
+            output = model.forward(inputs)
+            # Calculate Loss
+            valloss = criterion(output, labels)
+            # Add loss to the validation set's running loss
+            val_loss += valloss.item() * inputs.size(0)
+
+            # Since our model outputs a LogSoftmax, find the real
+            # percentages by reversing the log function
+            output = torch.exp(output)
+            # Get the top class of the output
+            top_p, top_class = output.topk(1, dim=1)
+            # See how many of the classes were correct?
+            equals = top_class == labels.view(*top_class.shape)
+            # Calculate the mean (get the accuracy for this batch)
+            # and add it to the running accuracy for this epoch
+            accuracy += torch.mean(equals.type(torch.FloatTensor)).item()
+
+            # Print the progress of our evaluation
+            counter += 1
+            print(counter, "/", len(val_loader))
+
+    # Get the average loss for the entire epoch
+    train_loss = train_loss / len(train_loader.dataset)
+    valid_loss = val_loss / len(val_loader.dataset)
+    # Print out the information
+    print('Accuracy: ', accuracy / len(val_loader))
+    print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(epoch, train_loss, valid_loss))
+
+########################################################################################################################
