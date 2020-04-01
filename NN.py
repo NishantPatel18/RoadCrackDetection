7717f725817ec532cf0f37f14ebe0c6426f47add
Nishant Patel <nishantpatel1842@gmail.com>
2020-04-01 14:51:27 +1300

    Updated NN and Testing on Random Image

--- NN.py
+++ NN.py
@@ -9,6 +9,7 @@ import numpy as np
 from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
+import time
 
 # Specify transforms using torchvision.transforms as transforms library
 transformations = transforms.Compose([
@@ -55,6 +56,7 @@ criterion = nn.NLLLoss()
 optimizer = optim.Adam(model.classifier.parameters())
 
 # Training the Model
+start_train = time.time()
 epochs = 2
 for epoch in range(epochs):
     train_loss = 0
@@ -84,8 +86,12 @@ for epoch in range(epochs):
         counter += 1
         print(counter, "/", len(train_loader))
 
+    end_train = time.time()
+    print('Finished Training in %0.2f minutes' % ((end_train - start_train) / 60))
+
 # Validating the Model
 
+    start_val = time.time()
     # Evaluating the model
     model.eval()
     counter = 0
@@ -116,6 +122,9 @@ for epoch in range(epochs):
             counter += 1
             print(counter, "/", len(val_loader))
 
+    end_val = time.time()
+    print('Finished Validating in %0.2f minutes' % ((end_val - start_val) / 60))
+
     # Get the average loss for the entire epoch
     train_loss = train_loss / len(train_loader.dataset)
     valid_loss = val_loss / len(val_loader.dataset)
@@ -123,4 +132,92 @@ for epoch in range(epochs):
     print('Accuracy: ', accuracy / len(val_loader))
     print('Epoch: {} \tTraining Loss: {:.6f} \tValidation Loss: {:.6f}'.format(epoch, train_loss, valid_loss))
 
+    print('Total Time for Training and Validating in %0.2f minutes' % ((end_val - start_train) / 60))
+
 ########################################################################################################################
+
+# Using the model
+
+model.eval()
+
+
+# Process our image
+def process_image(image_path):
+    # Load Image
+    img = Image.open(image_path)
+
+    # Get the dimensions of the image
+    width, height = img.size
+
+    # Resize by keeping the aspect ratio, but changing the dimension
+    # so the shortest size is 255px
+    img = img.resize((255, int(255 * (height / width))) if width < height else (int(255 * (width / height)), 255))
+
+    # Get the dimensions of the new image size
+    width, height = img.size
+
+    # Set the coordinates to do a center crop of 224 x 224
+    left = (width - 224) / 2
+    top = (height - 224) / 2
+    right = (width + 224) / 2
+    bottom = (height + 224) / 2
+    img = img.crop((left, top, right, bottom))
+
+    # Turn image into numpy array
+    img = np.array(img)
+
+    # Make the color channel dimension first instead of last
+    img = img.transpose((2, 0, 1))
+
+    # Make all values between 0 and 1
+    img = img / 255
+
+    # Normalize based on the preset mean and standard deviation
+    img[0] = (img[0] - 0.485) / 0.229
+    img[1] = (img[1] - 0.456) / 0.224
+    img[2] = (img[2] - 0.406) / 0.225
+
+    # Add a fourth dimension to the beginning to indicate batch size
+    img = img[np.newaxis, :]
+
+    # Turn into a torch tensor
+    image = torch.from_numpy(img)
+    image = image.float()
+    return image
+
+
+# Using our model to predict the label
+def predict(image, model):
+    # Pass the image through our model
+    output = model.forward(image)
+
+    # Reverse the log function in our output
+    output = torch.exp(output)
+
+    # Get the top predicted class, and the output percentage for
+    # that class
+    probs, classes = output.topk(1, dim=1)
+    return probs.item(), classes.item()
+
+
+# Show Image
+def show_image(image):
+    # Convert image to numpy
+    image = image.numpy()
+
+    # Un-normalize the image
+    image[0] = image[0] * 0.226 + 0.445
+
+    # Print the image
+    fig = plt.figure(figsize=(25, 4))
+    plt.imshow(np.transpose(image[0], (1, 2, 0)))
+
+
+# Process Image
+image = process_image("20000.jpg")
+# Give image to model to predict output
+top_prob, top_class = predict(image, model)
+# Show the image
+show_image(image)
+# Print the results
+print("The model is ", top_prob*100, "% certain that the image has a predicted class of ", top_class)
