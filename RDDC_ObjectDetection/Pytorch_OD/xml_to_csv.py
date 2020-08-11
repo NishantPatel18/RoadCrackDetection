df424b35b73ad99569f9fb4f454105a82d67ae47
Zaw Lin Tun <49043498+ZawLinTun16925410@users.noreply.github.com>
2020-08-11 16:22:01 +1200

    Update xml_to_csv.py

--- RDDC_ObjectDetection/Pytorch_OD/xml_to_csv.py
+++ RDDC_ObjectDetection/Pytorch_OD/xml_to_csv.py
@@ -26,9 +26,9 @@ def xml_to_csv(path):
 
 
 def main():
-    image_path = os.path.join(os.getcwd(), 'annotations')
+    image_path = os.path.join(os.getcwd(), '/content/RoadCrackDetection/RDDC_ObjectDetection/RoadDamageDataset/10_Annotations_Adachi')
     xml_df = xml_to_csv(image_path)
-    xml_df.to_csv('raccoon_labels.csv', index=None)
+    xml_df.to_csv('road_10_labels.csv', index=None)
     print('Successfully converted xml to csv.')
 
 
