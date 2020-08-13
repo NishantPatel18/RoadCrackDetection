370f00e0e7884f4ce321ae15a63782935b1fc84a
Zaw Lin Tun <49043498+ZawLinTun16925410@users.noreply.github.com>
2020-08-14 11:46:28 +1200

    Update Test.py

--- RDDC_ObjectDetection/Pytorch_OD/Test.py
+++ RDDC_ObjectDetection/Pytorch_OD/Test.py
@@ -1,34 +1,40 @@
 from PIL import Image
 from PIL import ImageDraw
 
-loaded_model = get_model(num_classes=5)
+loaded_model = get_model(num_classes=8)
 loaded_model.load_state_dict(torch.load("/content/RoadCrackDetection/RDDC_ObjectDetection/Pytorch_OD/road_crack/model"))
 
-idx = 0
-img, _ = dataset_test[idx]
-label_boxes = np.array(dataset_test[idx][1]["boxes"])
+def visual_image(index_of_image):
+    img, _ = dataset_test[index_of_image]
+    label_boxes = np.array(dataset_test[index_of_image][1]["boxes"])
 
-# put the model in evaluation mode
-loaded_model.eval()
+    # put the model in evaluation mode
+    loaded_model.eval()
 
-with torch.no_grad():
-    prediction = loaded_model([img])
+    with torch.no_grad():
+      prediction = loaded_model([img])
 
-image = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
-draw = ImageDraw.Draw(image)
+    image = Image.fromarray(img.mul(255).permute(1, 2, 0).byte().numpy())
+    draw = ImageDraw.Draw(image)
 
-# draw groundtruth
-for elem in range(len(label_boxes)):
-    draw.rectangle([(label_boxes[elem][0], label_boxes[elem][1]), (label_boxes[elem][2], label_boxes[elem][3])],
-                   outline="green", width=3)
 
-for element in range(len(prediction[0]["boxes"])):
-    boxes = prediction[0]["boxes"][element].cpu().numpy()
-    score = np.round(prediction[0]["scores"][element].cpu().numpy(), decimals=4)
+    # draw groundtruth
+    for elem in range(len(label_boxes)):
+        draw.rectangle([(label_boxes[elem][0], label_boxes[elem][1]), (label_boxes[elem][2], label_boxes[elem][3])], outline="green", width=3)
 
-    # need to change from 0.1 to 0.9 after we trained with many images
-    if score > 0.1:
-        draw.rectangle([(boxes[0], boxes[1]), (boxes[2], boxes[3])], outline="red", width=3)
-        draw.text((boxes[0], boxes[1]), text=str(score))
+    for element in range(len(prediction[0]["boxes"])):
+        boxes = prediction[0]["boxes"][element].cpu().numpy()
+        score = np.round(prediction[0]["scores"][element].cpu().numpy(), decimals=4)
 
-image
+        if score > 0.9:
+            draw.rectangle([(boxes[0], boxes[1]), (boxes[2], boxes[3])], outline="red", width=3)
+            draw.text((boxes[0], boxes[1]), text=str(score))
+
+    display(image)
+
+def main():
+    for index in range(len(dataset_test)):
+        visual_image(index)
+        print(index, 'is done')
+
+main()
