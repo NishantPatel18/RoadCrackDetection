98f8d6e1f298c0db2a08fe71d10019d1a0fd107d
Nishant Patel <nishantpatel1842@gmail.com>
2020-08-20 15:11:09 +1200

    Added Keypoint Object Detection Model

---
+++ RDDC_ObjectDetection/Pytorch_OD/keypoint_save.py
@@ -0,0 +1,2 @@
+os.mkdir("/content/RoadCrackDetection/RDDC_ObjectDetection/Pytorch_OD/road_crack/")
+torch.save(model.state_dict(), "/content/RoadCrackDetection/RDDC_ObjectDetection/Pytorch_OD/road_crack/keypoint_model")
