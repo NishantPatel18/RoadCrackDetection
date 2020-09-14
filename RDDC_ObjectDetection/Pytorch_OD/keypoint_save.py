edff3391b285c039cdecb9987d6c45a66597ddb1
Nishant Patel <nishantpatel1842@gmail.com>
2020-09-14 21:48:40 +1200

    Delete Keypoint Model Files

--- RDDC_ObjectDetection/Pytorch_OD/keypoint_save.py
+++
@@ -1,2 +0,0 @@
-os.mkdir("/content/RoadCrackDetection/RDDC_ObjectDetection/Pytorch_OD/road_crack/")
-torch.save(model.state_dict(), "/content/RoadCrackDetection/RDDC_ObjectDetection/Pytorch_OD/road_crack/keypoint_model")
